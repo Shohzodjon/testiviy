34fb69f296ca29a66e7c197c6773e693787fb8a6
Shohzod <xorozovshohzod@gmail.com>
2024-01-08 22:22:35 +0300

    added style

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,19 +1,65 @@
-import '../assets/styles/auth-modal.css'
+import "../assets/styles/auth-modal.css";
+import { useState } from "react";
+const AuthModal = () => {
+  const initialPassword = "Oybek";
+  const [error, setError] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [close, setClose] = useState(false);
 
-const AuthModal=()=>{
-    return(
-        <section className="auth__modal">
-            <div className="modal__flex">
-                  <ul>
-                    <li><h4>Login</h4></li>
-                    <li><label htmlFor="user__name">User name</label></li>
-                    <li><input type="text" id="user__name" placeholder='Enter name ... ' /></li>
-                    <li><label htmlFor="user__password">User password</label></li>
-                    <li><input type="password" id="user__password" placeholder='Enter password ... '/></li>
-                    <li><button>Login</button></li>
-                  </ul>
-            </div>
-        </section>
-    )
-}
+  const handlePassword = (e) => {
+    setPassword(e);
+    setError(false);
+  };
+
+  const handleSubmit = () => {
+    if (initialPassword === password) {
+      setError(false);
+      setClose(true);
+    } else {
+      setError(true);
+      setClose(false);
+    }
+  };
+
+  return (
+    <section className={close ? "remove__modal" : "auth__modal"}>
+      <div className="modal__flex">
+        <ul>
+          <li>
+            <h4>Login</h4>
+          </li>
+          <li>
+            <label htmlFor="user__name">User name</label>
+          </li>
+          <li>
+            <input
+              type="text"
+              id="user__name"
+              placeholder="Enter name ... "
+              onChange={(e) => setName(e.target.value)}
+            />
+          </li>
+          <li>
+            <label htmlFor="user__password">User password</label>
+          </li>
+          <li>
+            <input
+              type="password"
+              id="user__password"
+              placeholder="Enter password ... "
+              onChange={(e) => handlePassword(e.target.value)}
+            />
+          </li>
+          <li>
+            <span className={error ? "error" : ""}>Something is wrong</span>
+          </li>
+          <li>
+            <button onClick={handleSubmit}>Login</button>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
 export default AuthModal;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,66 @@
-import img from '../assets/images/imge.png'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import img from "../assets/images/imge.png";
+import client from "../assets/images/client.png";
+import client2 from "../assets/images/client2.png";
+import client3 from "../assets/images/client3.png";
+import client4 from "../assets/images/client4.png";
+import client5 from "../assets/images/client5.png";
+import client6 from "../assets/images/client6.png";
+import client7 from "../assets/images/client7.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "../assets/styles/home-page.css";
 const HomePage = () => {
-    return (
-        <section className="home__page">
-            <div className="container">
-
-                <div className="home__page__flex">
-                    <div className="flex__left__side">
-                        <h1>Lessons and insights from <span>8 years</span></h1>
-                        <p>Where to grow your business as a photographer: site or social media?</p>
-                    </div>
-                    <div className="flex__right__side">
-                        <img src={img} alt="img" />
-                    </div>
-                </div>
-            </div>
-            <div className='client__section'>
-                <div className='container'>
-                    <h3>Our Clients</h3>
-                    <p>We have been working with some Fortune 500+ clients</p>
-                    <Swiper
-                        spaceBetween={50}
-                        slidesPerView={3}
-                    >
-                        <SwiperSlide>Slide 1</SwiperSlide>
-                        <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                    </Swiper>
-                </div>
-            </div>
-
-        </section>
-    )
-}
+  const imageArr = [
+    client,
+    client2,
+    client3,
+    client4,
+    client5,
+    client6,
+    client7,
+  ];
+  return (
+    <section className="home__page">
+      <div className="container">
+        <div className="home__page__flex">
+          <div className="flex__left__side">
+            <h1>
+              Lessons and insights from <span>8 years</span>
+            </h1>
+            <p>
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+          </div>
+          <div className="flex__right__side">
+            <img src={img} alt="img" />
+          </div>
+        </div>
+      </div>
+      <div className="client__section">
+        <div className="container">
+          <h3>Our Clients</h3>
+          <p>We have been working with some Fortune 500+ clients</p>
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={5}
+            loop={true}
+            modules={[Autoplay]}
+          >
+            {imageArr.map((img, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <div className="client__card">
+                    <img src={img} alt="img" />
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default HomePage;
